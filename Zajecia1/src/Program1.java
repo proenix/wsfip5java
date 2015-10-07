cfeffb812bec54ffda53ab8226914ee6ad1e565d
Proenix <piotr26ch@gmail.com>
2015-10-07 22:17:07 +0200

    Zajecia 1: wyswietlanie tekstu na ekran.

---
+++ Zajecia1/src/Program1.java
@@ -0,0 +1,18 @@
+import java.io.PrintWriter;
+
+public class Program1 {
+	
+	public static void main (String args[])
+	{
+		System.out.println("Witaj\nprzybyszu\n");
+		
+		String s = new String("Jakiœ tekst");
+		System.out.println(s);
+		
+		PrintWriter stdout = new PrintWriter(System.out,true);
+		stdout.println(s);
+		
+		System.out.println(args[0] + " " + args[1]);
+	}
+	
+}
