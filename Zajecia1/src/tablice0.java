c5972bf8a2fc1e703364fb58f20602ca4ea0b3b6
Proenix <piotr26ch@gmail.com>
2015-10-11 14:59:35 +0200

    Deklaracja tablicy.

---
+++ Zajecia1/src/tablice0.java
@@ -0,0 +1,14 @@
+
+public class tablice0 {
+	public static void main (String args[]) {
+		
+		int[] anArray;
+		anArray = new int[10];
+		
+		anArray[0] = 100;
+		anArray[2] = 100;
+		
+		System.out.println(anArray[0]);
+	}
+	
+}
