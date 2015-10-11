7f14cb6edd24775e7626f95ccf2ca8f90e45c60a
Proenix <piotr26ch@gmail.com>
2015-10-11 15:09:30 +0200

    Tablica 2 wymiarowa.

---
+++ Zajecia1/src/tablice1.java
@@ -0,0 +1,13 @@
+
+public class tablice1 {
+	public static void main (String args[]) {
+		
+		String[][] nazwy = {
+				{"Pan", "Państwo", "Pani"},
+				{"Kowalski", "Kowalscy", "Kowalska"}
+		};
+		
+		System.out.println(nazwy[0][0] + nazwy[1][0]);
+		System.out.println(nazwy[1][0] + nazwy[1][0]);
+	}
+}
