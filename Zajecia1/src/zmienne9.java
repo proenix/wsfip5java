6b26b6da7b4a8d7cb03c5b4363e698272acf5c5e
Proenix <piotr26ch@gmail.com>
2015-10-11 14:10:16 +0200

    Przykład pętli while.

---
+++ Zajecia1/src/zmienne9.java
@@ -0,0 +1,13 @@
+
+public class zmienne9 {
+	public static void main (String args[]) {
+		
+		int count=1;
+		while(count < 11) {
+			// Wyplucie aktualnego counta
+			System.out.println(count);
+			// Post inkrementacja zmiennej count
+			count++;
+		}
+	}
+}
