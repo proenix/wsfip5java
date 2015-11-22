8dd8dacc8119acf080e2d79533a575f21ae3f8b5
Proenix <piotr26ch@gmail.com>
2015-11-22 16:38:53 +0100

    Tworzenie elementu - przycisk

---
+++ Zajecia2/src/Applet01_5.java
@@ -0,0 +1,17 @@
+import java.applet.*;
+import java.awt.Button;
+
+public class Applet01_5 extends Applet {
+	
+	public void init() {
+		
+		Button button1 = new Button("Button 1");
+		Button button2 = new Button("Button 1");
+		Button button3 = new Button("Button 1");
+		
+		add(button1);
+		add(button2);
+		add(button3);
+	}
+
+}
