ae19c29d05d7c10a92c7e5d2568b96f459006722
Proenix <piotr26ch@gmail.com>
2016-01-09 16:14:17 +0100

    Listener do listy

--- Zajecia4/src/Applet07_Choice1.java
+++ Zajecia4/src/Applet07_Choice1.java
@@ -1,17 +1,49 @@
 import java.applet.*;
 import java.awt.Choice;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class Applet07_Choice1 extends Applet {
-	
+public class Applet07_Choice1 extends Applet implements ItemListener {
+
 	private Choice kolor = new Choice();
-	
+
 	public void init() {
 		kolor.add("Biały"); //0
 		kolor.add("Czarny"); //1
 		kolor.add("Czerwony"); //2
 		kolor.add("Niebieski"); //3
-		kolor.add("Przezroczysty");//4
-		
+		kolor.add("Szary");//4
+
+		kolor.addItemListener(this);
 		add(kolor);
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+
+		switch(kolor.getSelectedIndex()) {
+			case 0:
+				setBackground(Color.white);
+				break;
+			case 1:
+				setBackground(Color.yellow);
+				break;
+			case 2:
+				setBackground(Color.red);
+				break;
+			case 3:
+				setBackground(Color.blue);
+				break;
+			case 4:
+				setBackground(Color.gray);
+				break;
+		}
+	}
+
+	public void paint(Graphics g) {
+		int items = kolor.getItemCount();
+		g.drawString("Lista zawiera: " + items + " wpisy", 10, 70);
+		g.drawString("Ustawiono kolor: " + kolor.getSelectedItem(), 10, 90);
 	}
 }
